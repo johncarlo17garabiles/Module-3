2504b87481c6fdb357d8245794cbde1d9ebea086
johncarlo17garabiles <114630003+johncarlo17garabiles@users.noreply.github.com>
2022-09-29 11:10:40 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public class Circle
+        {
+            static void Main(string[] args)
+            {
+                double radius, area;
+                double PI = 3.1415;
+                Console.WriteLine("Enter the radius of the circle: ");
+                radius = Convert.ToDouble(Console.ReadLine());
+                area = PI * radius *radius;
+
+                Console.WriteLine("Radius of circle is {0}, And", radius);
+                Console.WriteLine("The area of circle with radius {0} is {1}", radius,area);
+            }
+        }
+    }
+}
